ccbea5a0dacb2e7d0c4fb015335e0a52ad7be80f
violetaruizmartinez <uo258454@uniovi.es>
2019-02-08 14:11:55 +0100

    SDI - 1.8 Trabajando con controladores

---
+++ src/main/java/com/uniovi/entities/Mark.java
@@ -0,0 +1,30 @@
+package com.uniovi.entities;
+
+public class Mark {
+	private Long id;
+	private String description;
+	private Double score;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+}
