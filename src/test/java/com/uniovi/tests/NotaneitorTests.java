0d800ce8962eb8981d6c271d3a3eb9556f0fe3c8
violetaruizmartinez <uo258454@uniovi.es>
2019-02-28 10:13:53 +0100

    SDI-Selenium-5.4-CasosUso-VistaHome

--- src/test/java/com/uniovi/tests/NotaneitorTests.java
+++ src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -9,14 +9,16 @@ import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.uniovi.tests.pageobjects.PO_HomeView;
+import com.uniovi.tests.pageobjects.PO_Properties;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class NotaneitorTests {
 
 	// EnWindows (Debe ser la versión 65.0.1 y
 	// desactivar las actualizacioens automáticas)):
-	static String PathFirefox65 = "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe";
-	static String Geckdriver022 = "C:\\Users\\uo258454\\Desktop\\S5material\\PL-SDI-S5-material\\geckodriver024win64.exe";
+	static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+	static String Geckdriver022 = "C:\\Users\\Violeta\\Desktop\\SDI sesion 5 material\\PL-SDI-Sesión5-material\\geckodriver024win64.exe";
 	// En MACOSX (Debe ser la versión 65.0.1y desactivar las actualizacioens
 	// automáticas):
 	// staticString
@@ -41,22 +43,44 @@ public class NotaneitorTests {
 
 	@After
 	public void tearDown() throws Exception {
-		
-		
+
 		driver.manage().deleteAllCookies();
 	}
-	
+
 	@BeforeClass
-	public void begin() {}
-	
+	public static void begin() {
+	}
+
 	@AfterClass
-	public void end() {
+	public static void end() {
 		driver.quit();
 	}
 
+	// PR01. Acceder a la página principal /
 	@Test
-	public void test() {
-		fail("Not  yet  implemented");
+	public void PR01() {
+		PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH());
 	}
 
+	// PR02. OPción de navegación. Pinchar en el enlace Registro en la página home
+	@Test
+	public void PR02() {
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+	}
+
+	// PR03. OPción de navegación. Pinchar en el enlace Identificate en la página
+	// home
+	@Test
+	public void PR03() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	}
+
+	// PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a
+	// Español
+	@Test
+	public void PR04() {
+		PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(),
+				PO_Properties.getENGLISH());
+		// SeleniumUtils.esperarSegundos(driver, 2);
+	}
 }
